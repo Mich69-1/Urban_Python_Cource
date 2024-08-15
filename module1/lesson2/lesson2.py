35d2a33efc95d32cf59548f5c0cf285da8a59cbd
Mich69-1 <berlandme@gmail.com>
2024-08-16 00:29:45 +0500

    Second lesson done

---
+++ module1/lesson2/lesson2.py
@@ -0,0 +1,10 @@
+# Практическое задание по теме "Переменные"
+
+number_of_homeworks = 12
+hours_spent = 1.5
+course_title = "Python"
+# Вычисляем время на одно задание
+time_per_homework = 1.5/12
+# Выводим результат
+print("Курс:", course_title, ", всего задач:", number_of_homeworks, ", затрачено часов:",
+      hours_spent, ", среднее время выполнения", time_per_homework, "часа.")
