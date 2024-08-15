adc9ef3424d89fee538800cfbde2a70d56c6e166
Mich69-1 <berlandme@gmail.com>
2024-08-15 07:30:15 +0500

    First lesson done

--- module1/lesson1/lesson1.py
+++ module1/lesson1/lesson1.py
@@ -1,1 +1,14 @@
+# Практическое задание по уроку "Строки и индексация строк"
 
+# пример 1
+example = "Абракадабра"
+# пример 2
+print(example[0])
+# пример 3
+print(example[-1])
+# пример 4
+print(example[int(len(example)/2):])
+# пример 5
+print(example[::-1])
+# пример 6
+print(example[1::2])
