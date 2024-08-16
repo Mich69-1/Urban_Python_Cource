b615a0b01c26afe8255bc09aef2e0e09c45e3d1d
Mich69-1 <berlandme@gmail.com>
2024-08-16 18:03:18 +0500

    3rd done

---
+++ module1/lesson3/module_1_3.py
@@ -0,0 +1,14 @@
+# Практическая работа по уроку "Динамическая типизация"
+
+name = "Михаил"
+print("Name:", name)
+age = 55
+print("Age:", age)
+age = age+1
+print("New Age:", age)
+is_student = True
+print("Is Student:", is_student)
+
+
+
+
