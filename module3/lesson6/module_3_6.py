c6e3e5297c0b9e4db9183873d3c43c8e4a68ec1d
Mich69-1 <berlandme@gmail.com>
2024-09-02 18:42:43 +0500

    mod 3 leeson_6 temp1

--- module3/lesson6/module_3_6.py
+++ module3/lesson6/module_3_6.py
@@ -1,4 +1,26 @@
-import copy
+# Дополнительное практическое задание по модулю: "Подробнее о функциях."
+
+def expand_and_sum(data_s):
+    if any(isinstance(sub, (list, tuple, dict, set)) for sub in data_s):
+        elem = data_s.pop(0)
+        if isinstance(elem, (list, tuple, dict, set)):
+            if isinstance(elem, dict):
+                data_s.extend(list(elem.items()))
+            else:
+                data_s.extend(list(elem))
+            return expand_and_sum(data_s)   # return обязателен!
+        else:
+            data_s.append(elem)
+            return expand_and_sum(data_s)
+    else:
+        structure_sum = 0
+        for i in data_s:
+            if isinstance(i, (int, float)):
+                structure_sum += i
+            else:
+                structure_sum += len(i)
+        return structure_sum
+
 
 data_structure = [
     [1, 2, 3],
@@ -7,22 +29,6 @@ data_structure = [
     "Hello",
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-
-
-def expand_and_sum(data_s):
-    data_copy = copy.deepcopy(data_s)
-    if any(isinstance(sub, (list, tuple, dict, set)) for sub in data_copy):
-        elem = data_copy.pop(0)
-        if isinstance(elem, (list, tuple, dict, set)):
-            data_copy.extend(list(elem))
-            expand_and_sum(data_copy)
-        else:
-            data_copy.append(elem)
-            expand_and_sum(data_copy)
-    else:
-        print(data_copy)
-        return data_copy
-
-
 result = expand_and_sum(data_structure)
 print(result)
+print(data_structure)
