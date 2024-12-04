ff4fe2ce8dce2fa170094cb37c1a11d5ac4afc70
Mich69-1 <berlandme@gmail.com>
2024-12-04 23:06:30 +0500

    mod 12 lesson_1 done

--- module12/lesson1/module_12_1.py
+++ module12/lesson1/module_12_1.py
@@ -2,3 +2,25 @@
 
 from runner import Runner
 import unittest
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        runner = Runner('Вася')
+        [runner.walk() for i in range(10)]
+        self.assertEqual(runner.distance,  50)
+
+    def test_run(self):
+        runner = Runner('Петя')
+        [runner.run() for i in range(10)]
+        self.assertEqual(runner.distance,  100)
+
+    def test_challenge(self):
+        runner = Runner('Саша')
+        runner1 = Runner('Петя')
+        [(runner.run(), runner1.walk()) for i in range(10)]
+        self.assertNotEqual(runner.distance, runner1.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
